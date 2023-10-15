ab1346d9e51c569a451157e64409022679a0ffca
utter-whisky <nhcadoizay92@outlook.com>
2023-10-15 16:36:14 +0800

    third commit

--- src/main/java/GitTest/java/GitTest.java
+++ src/main/java/GitTest/java/GitTest.java
@@ -3,5 +3,6 @@ package GitTest.java;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
